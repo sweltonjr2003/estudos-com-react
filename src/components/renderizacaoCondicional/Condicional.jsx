8916845ce8951938ee1e703394c7cbe71e0496e9
Swelton <114576001+sweltonjr2003@users.noreply.github.com>
2023-01-07 12:54:54 +0100

    renderização condicional

--- src/components/renderizacaoCondicional/Condicional.jsx
+++ src/components/renderizacaoCondicional/Condicional.jsx
@@ -6,29 +6,12 @@ import './Formulario.css'
 import Botao from './Botao'
 
 const Condicional = () => {
-    let print
-
     const [email, setEmail] = useState()
+    const [userEmail, setUserEmail] = useState()
 
     const enviarEmail = (e) => {
         e.preventDefault()
-        
-
-        if(email) {
-            print = (
-                <div>
-                    <p>O e-mail do utilizador é: {email}.</p>
-                </div>
-            )
-        } else {
-            print = (
-                <div>
-                    <p>O e-mail ainda não foi enviado!!</p>
-                </div>
-            )
-        }
-
-        console.log(print)
+        setUserEmail(email)
     }
 
     return (
@@ -38,6 +21,11 @@ const Condicional = () => {
                 <input type="email" name="email" id="email" placeholder='Digite o seu e-mail...' onChange={(e) => setEmail(e.target.value)} />
                 <Botao evento={enviarEmail} texto={'Enviar o e-mail'} />
             </form>
+            {userEmail && (
+                <div>
+                    <p>O e-mail de utilizador é: {userEmail}</p>
+                </div>
+            )}
         </div>
     )
 }
